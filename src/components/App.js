866bc9f22d57cf48a8ec0d49bc4cbb2f7dbccf31
jks6404 <jannusingh.75@gmail.com>
2023-08-27 07:32:23 +0530

    submitting code-Sun Aug 27 2023 07:32:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import Sum from "./Sum";
+
+
 const App = () => {
-  let [sum,setSum]=useState(0);
+
+  let [sum,setSum] = useState(0);
+
+  const updateSum = (e)=>{
+    let value = parseFloat(e.target.value);
+    setSum((prevState)=> prevState + value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Sum Calculator</h1>
-        <Sum sum={sum} setSum={setSum}/>
-          <p>Sum: {sum}</p>
+        <input type="number" onChange={(e)=>updateSum(e)}/>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
 
-export default App;
+export default App
